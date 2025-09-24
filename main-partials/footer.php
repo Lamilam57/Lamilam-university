f6a73588e10b1a1e5734d89365877ff0d635706e
Lamilam57 <112189009+Lamilam57@users.noreply.github.com>
2025-09-24 04:42:19 -0700

    Add files via upload

---
+++ main-partials/footer.php
@@ -0,0 +1,18 @@
+<section class="footer">
+            <h4>ABOUT US</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dolor aliquet turpis nulla, eleifend <br>fauctibus est sollicitudin ut. Maecenas ut venenatis ex, et depibus purus.</p>
+        </section>
+        
+        
+        <script>
+            var navLinks=document.getElementById("navLinks");
+            
+            function showMenu(){
+                navLinks.style.right= "0";               
+            }
+            function hideMenu(){
+                navLinks.style.right= "-200px";
+            }
+        </script>
+    </body>
+</html>
---
+++ main-partials/menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>university website design</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <meta name="viewport" content="width=device-width, inital-scale=1.0 ">
+    </head>
+    
+    <body>
+        <section class="sub-header">
+            <nav>
+                <a href="index.php"><img src="img/logo.png"></a>
+                <div class="nav-links" id="navLinks">
+                    <button type="button" class="btn" id="btnc" onclick="hideMenu()"><a>&times;</a></button>
+                    <ul>
+                        <li><a href="index.php">home</a></li>
+                        <li><a href="about.php">about</a></li>
+                        <li><a href="course.php">course</a></li>
+                        <li><a href="blog.php">blog</a></li>
+                        <li><a href="contact.php">contact</a></li>
+                        
+                    </ul>
+                </div>
+                <button type="button" class="btn" onclick="showMenu()"><a>&#9776;</a></button>
+            </nav>
+            
+            <!--<h1>About Us</h1>-->
+        </section>
